1c7a723772032352d1fee2ab7c088870b67fd64b
snorkem <snorkem@gmail.com>
2020-03-02 11:01:06 -0800

    added checks for 'WA' and 'CC' prefixes, plus arguments

--- Tapeless_Media_Inventory_Pro_S41.py
+++ Tapeless_Media_Inventory_Pro_S41.py
@@ -2,12 +2,13 @@
 import difflib
 import csv
 from pathlib import Path
+import argparse
 import subprocess
 import os
 import shutil
 from datetime import datetime
 ######### Global Variables #########
-camera_keywords = ['RED', 'BLK', 'DRN', 'AMI', 'ADC', 'CC', 'OSM', 'SA7', 'SD', 'WAV', 'WA', 'TTL', 'SD', 'A7S',
+camera_keywords = ['RED', 'BLK', 'DRN', 'AMI', 'ADC', 'CC', 'OSM', 'SA7', 'SD', 'WAV', 'WA', 'TTL', 'A7S',
                    'GPR']
 working_dir = Path('~/tapelist').expanduser()
 reports_dir = Path(working_dir) / 'zOld_Reports'
@@ -15,7 +16,6 @@ PATH_TO_G_RACK = Path('/Users/Alex/Desktop/ODA/')
 output_html = 'Tapelist_Compare_by_Camera.html'
 dt_string = datetime.now().strftime("%Y-%m-%d_%HH-%MM-%SS")
 output_path = str(working_dir / '{time}_{file_name}'.format(time=dt_string, file_name=output_html))
-
 ######### End Global Variables #########
 
 
@@ -56,14 +56,14 @@ def get_tapes_from_csv(csv_file):
             print('Something is wrong with the CSV file. Check that it is formatted properly with the tape'
                   'names in an "ID" column. Quiting...')
             exit(0)
-    inventory_ids.sort(reverse=True)
+    inventory_ids.sort(reverse=False)
     return inventory_ids
 
 
 def get_tapes_from_path(path):
     newpath = Path(path).glob('*/*')
     tape_names = [item.name for item in newpath if item.is_dir()]
-    tape_names.sort(reverse=True)
+    tape_names.sort(reverse=False)
     #for tape in tapenames:
     #    print('tape item is: ' + tape)
     return tape_names
@@ -73,21 +73,23 @@ def get_tapes_by_camera(tape_list: list, camera_list: list):
     tapes = []
     final_tape_list = []
     for camera in camera_list:
-        if camera != 'WAV' and camera != 'WA':
+        if camera != 'WA':
             for tape in tape_list:
-                if camera in tape:
+                if camera in tape[:3]:
                     tapes.append(tape)
-        else:
+        elif camera == 'WA':
             for tape in tape_list:
-                if camera == tape[:3]:
+                if camera == tape[-2:]:
                     tapes.append(tape)
-                elif camera == tape[:2] and tape[2] != 'V':
+        elif camera == 'CC':
+            for tape in tape_list:
+                if camera == tape[:2]:
                     tapes.append(tape)
     # for tape in tapes_by_camera:
         # clean it up similar to Asher's regex
         # append to final_tape_list
     # sort
-    tapes.sort(reverse=True)
+    tapes.sort(reverse=False)
     return tapes
 
 
@@ -116,13 +118,19 @@ def write_diff_table(local_tapes, inventory_tapes):
 
 def main():
     is_g_rack_connected(PATH_TO_G_RACK)
-    print("In order to run a check between the G-Rack and the DELTA Spire inventory, you will need\n"
-          "to export a CVS file from DELTA Spire. Once you've done that, enter the path, or drag the file\n"
-          "into the terminal, then press enter. The results will appear in ~/tapelist and should open in your\n"
-          "web browser automatically.\n\n")
+    # Create the parser
+    my_parser = argparse.ArgumentParser(description='Check tapeless media against inventory.')
+    # Add the arguments
+    my_parser.add_argument('-csv',
+                           metavar='csv',
+                           type=Path,
+                           help='the path to list')
+    # Execute the parse_args() method
+    args = my_parser.parse_args()
+    csv_file = args.csv # Get CSV file from user argument
     if reports_dir.exists() is False:
         reports_dir.mkdir(parents=True)
-    csv_file = get_file_path() # Check is csv file is valid and return path
+    # csv_file = get_file_path() # Check is csv file is valid and return path
     while is_csv_valid(csv_file) is False:
         print('Invalid path or file. Try again.')
         csv_file = get_file_path()
